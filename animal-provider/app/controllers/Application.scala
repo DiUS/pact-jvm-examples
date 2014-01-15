c048410fec15ad0d6b25458e701fbdb5ab3647b5
thetrav <the.trav@gmail.com>
2014-01-15 11:12:22 +1100

    verify against more pacts

--- animal-provider/app/controllers/Application.scala
+++ animal-provider/app/controllers/Application.scala
@@ -1,15 +1,30 @@
 package controllers
 
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, Json}
 
 object Application extends Controller {
 
+  private var state: String = "default"
+
+  private val fixtures = Map(
+    "default" -> InternalServerError("state not set"),
+    "there are alligators" -> Ok(Json.arr(Json.obj("name" -> "Bob"))),
+    "an error has occurred" -> InternalServerError(Json.obj("error" -> "Argh!!!")),
+    "there is an alligator named Mary" -> Ok(Json.obj("name" -> "Mary")),
+    "there is not an alligator named Mary" -> NotFound
+  )
+
   def index = Action {
-    Ok(Json.arr(Json.obj("name" -> "Bob")))
+    fixtures(state)
   }
 
-  def enterState = Action {
+  def find(name: String) = Action {
+    fixtures(state)
+  }
+
+  def enterState = Action(parse.json) { request =>
+    state = (request.body \ "state").as[String]
     Ok
   }
 
